071302aa36c5312aefc66cc27de07eff6e2c6535
Shrestha Jaiswal <78994771+sj1705@users.noreply.github.com>
2023-02-15 02:15:21 +0530

    Time: 3 ms (81.49%), Space: 8.1 MB (85.69%) - LeetHub

--- 1550-three-consecutive-odds/1550-three-consecutive-odds.cpp
+++ 1550-three-consecutive-odds/1550-three-consecutive-odds.cpp
@@ -13,23 +13,23 @@
 // };
 
 
-// class Solution {
-// public:
-//     bool threeConsecutiveOdds(vector<int>& arr) {
-//     int odds = 0;
-//     for (auto i = 0; i < arr.size() && odds < 3; ++i)
-//         odds = arr[i] % 2 ? odds + 1 : 0;
-//     return odds == 3;
-// }
-// };
-
 class Solution {
 public:
-	bool threeConsecutiveOdds(vector<int>& arr) {
-		int n = arr.size();
-		for(int i = 0 ; i < n - 2 ; i++){
-			if(arr[i]%2 && arr[i+1]%2 && arr[i+2]%2) return true;
-		}
-		return false;
-	}
+    bool threeConsecutiveOdds(vector<int>& arr) {
+    int odds = 0;
+    for (auto i = 0; i < arr.size() && odds < 3; ++i)
+        odds = arr[i] % 2 ? odds + 1 : 0;
+    return odds == 3;
+}
 };
+
+// class Solution {
+// public:
+// 	bool threeConsecutiveOdds(vector<int>& arr) {
+// 		int n = arr.size();
+// 		for(int i = 0 ; i < n - 2 ; i++){
+// 			if(arr[i]%2 && arr[i+1]%2 && arr[i+2]%2) return true;
+// 		}
+// 		return false;
+// 	}
+// };
