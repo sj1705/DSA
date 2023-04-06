d53e1c386f7b0c13200fc41fc8c6ed635c0b755f
Shrestha Jaiswal <78994771+sj1705@users.noreply.github.com>
2023-04-06 09:20:47 +0530

    Time: 2 ms (48.22%), Space: 7.1 MB (76.43%) - LeetHub

--- 2465-number-of-distinct-averages/2465-number-of-distinct-averages.cpp
+++ 2465-number-of-distinct-averages/2465-number-of-distinct-averages.cpp
@@ -3,7 +3,6 @@ public:
     int distinctAverages(vector<int>& nums) {
         unordered_set<float> s;
         sort(nums.begin(), nums.end()); 
-        
         int i = 0, j = size(nums)-1;
         while (i < j) 
             s.insert((float)(nums[i++] + nums[j--])/2);
