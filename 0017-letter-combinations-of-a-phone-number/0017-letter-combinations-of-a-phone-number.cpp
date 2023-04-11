59279d3f5522e0d40ddcb84cf1b1d18a87f30372
Shrestha Jaiswal <78994771+sj1705@users.noreply.github.com>
2023-04-11 23:12:01 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (50.13%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -1,32 +1,26 @@
 class Solution {
+private:
+    void letterCombinations(string digits, vector<string>& output, string &temp, vector<string>& pad, int index){
+        if(index == digits.size()){
+            output.push_back(temp);
+            return;
+        }
+        string value = pad[digits[index]-'0'];
+        for(int i=0; i<value.size(); i++){
+            temp.push_back(value[i]);
+            letterCombinations(digits, output, temp, pad, index+1);
+            temp.pop_back();
+        }
+    }
 public:
-      const vector<string> pad = {
-        "", "", "abc", "def", "ghi", "jkl",
-        "mno", "pqrs", "tuv", "wxyz"
-    };
-vector<string> ans;
-void fun(int i,string cstr,string str){
-    if(i==str.size()){
-        ans.push_back(cstr);
-        return ;
-    }
-
-    for(auto it:pad[str[i]-'0']){
- 
-        
-            string nstr=cstr+it;
-            fun(i+1,nstr,str);
-            nstr.pop_back();
-        
-    }
-    return ;
-}
-    vector<string> letterCombinations(string str) {
- 
-      
-            fun(0,"",str);
-            if(str.size()==0) return {};
-            return ans;
-
+    vector<string> letterCombinations(string digits) {
+        if(digits.empty()){
+            return {};
+        }
+        vector<string> pad = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        vector<string> output;
+        string temp;
+        letterCombinations(digits, output, temp, pad, 0);
+        return output;
     }
 };
