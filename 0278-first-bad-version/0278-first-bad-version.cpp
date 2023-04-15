9870a72b1d29ca5e448b2b9c0163ecc1522c495c
Shrestha Jaiswal <78994771+sj1705@users.noreply.github.com>
2023-04-15 21:21:56 +0530

    Time: 3 ms (45.47%), Space: 6 MB (8.15%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.cpp
+++ 0278-first-bad-version/0278-first-bad-version.cpp
@@ -1,20 +1,22 @@
 class Solution {
 public:
     int firstBadVersion(int n) {
-         int s = 0, e = n;
-
-         int minBadVersion = n;
-
-        while(s <= e){
-             int mid = s + (e-s) / 2;
-            if(isBadVersion(mid)){
-                minBadVersion = mid;
-                e = mid - 1;
-            }else{
-                s = mid + 1;
+        int left = 1, right = n, ans, mid;
+        while(left <= right)
+        {
+            mid = left + (right - left)/2;
+            if(isBadVersion(mid) == false)
+                left = mid + 1;
+            else
+            {
+                ans = mid;
+                if(mid == 1 || isBadVersion(mid-1) == false) 
+                {
+                    return mid;
+                }
+                right = mid - 1;
             }
         }
-
-        return minBadVersion;
+        return ans;
     }
 };
