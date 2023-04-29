84aef526c5f8a013c8d783c1ed852de300fe5ba8
Shrestha Jaiswal <78994771+sj1705@users.noreply.github.com>
2023-04-29 23:08:04 +0530

    Time: 216 ms (87.54%), Space: 114.2 MB (88.21%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -1,27 +1,26 @@
 class Solution {
 public:
     bool isPalindrome(ListNode* head) {
-        // Some Brute Force Steps to follow
-        // 1) Reverse the list and store it in r_head;
-        // and then compare head->val and r_head->val if equal move both the pointers otherwise if not equal return false.
-        // When the loop will be executed fully retrun true
-         if(head == NULL || head->next == NULL){
-            return (head);
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while (fast != NULL && fast -> next != NULL) {
+            slow = slow -> next;
+            fast = fast -> next -> next;
         }
-        ListNode *r_head = NULL;
-        ListNode *ptr = head;
-        while(ptr!=NULL){
-            ListNode *temp = new ListNode(ptr->val);
-            temp ->next = r_head;
-            r_head = temp;
-            ptr = ptr->next;
+        if (fast != NULL && fast -> next == NULL) slow = slow -> next;
+        ListNode* prv = NULL;
+        ListNode* tmp = NULL;
+        while (slow != NULL) {
+            tmp = slow -> next;
+            slow -> next = prv;
+            prv = slow;
+            slow = tmp;
         }
-        while(head && r_head){
-            if(head->val != r_head->val){
-                return false;
-            }
-            head = head->next;
-            r_head = r_head->next;
+        slow = prv, fast = head;
+        while (slow && fast) {
+            if (slow -> val != fast -> val) return false;
+            slow = slow -> next;
+            fast = fast -> next;
         }
         return true;
     }
