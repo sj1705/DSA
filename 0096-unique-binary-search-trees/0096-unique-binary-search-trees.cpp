b09b1ec269f58c47d3c0f348e1f5f3fec8c72d48
Shrestha Jaiswal <78994771+sj1705@users.noreply.github.com>
2023-04-30 22:38:21 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (14.26%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
@@ -1,10 +1,27 @@
 class Solution {
 public:
-    int numTrees(int n) {
-        if(n <= 1) return 1;
-        int ans = 0;
-        for(int i = 1; i <= n; i++) 
-            ans += numTrees(i-1) * numTrees(n-i);
-        return ans;
+    vector<vector<int>> dp;
+    int solve(int i,int j)
+    {
+        if(i>j) return 1;
+        if(i==j) return dp[i][j]=1;
+        
+        if(dp[i][j]!=-1) return dp[i][j];
+        int sum=0;
+        for(int k=i;k<=j;k++)
+        {
+            if(dp[i][k-1]==-1)
+            dp[i][k-1]=solve(i,k-1);
+            if(dp[k+1][j]==-1)
+            dp[k+1][j]=solve(k+1,j);
+            sum+=dp[i][k-1]* dp[k+1][j];
+        }
+        return dp[i][j]=sum;
+    }
+    int numTrees(int n) 
+    {
+        dp.resize(22,vector<int>(22,-1));
+         solve(1,n);
+        return dp[1][n];
     }
 };
