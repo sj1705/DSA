fdf23925b42949c2a17169113a249edc97019f3d
Shrestha Jaiswal <shresthajaiswal1705@gmail.com>
2023-05-20 02:29:21 +0530

    f

--- pointer.cpp
+++ pointer.cpp
@@ -4,5 +4,20 @@ using namespace std;
 int main()
 {
     int num =5;
-    cout << num;
+    cout << num << endl;
+    // address of operator<<
+    cout << "This is Address of num:  "<< &num << endl;
+
+
+    // crearting pointer
+    int *p =&num;
+    cout << *p <<endl; // value at address of num
+    cout << p <<endl; // addrress of num
+
+    cout << "size of interger is :  " << sizeof(num) << endl;
+    cout << "size of pointer of int is :  " << sizeof(p) << endl;    
+
+
+    int *p2 = 0;
+    cout << "risky practice :   " << *p2 << endl;
 }
