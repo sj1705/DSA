aef59b8836e80d875c9bcafd95b44a2bf8e23612
Shrestha Jaiswal <shresthajaiswal1705@gmail.com>
2023-05-21 01:00:06 +0530

    ff

---
+++ araaypoainter.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int arr[10] = {2,5,6,7,3,5,5};
+    cout<< "Address of first block of array is  :    " << arr << endl;
+    cout<< "Address of first block of array is  :    " << &arr[0] << endl;
+    int a =5;
+    int *ptr=&a;
+    cout << *ptr << endl;
+    cout << *arr << endl;
+    cout << *(arr+1) << endl;
+    cout << a[arr] << endl;
+    return 0;
+}
