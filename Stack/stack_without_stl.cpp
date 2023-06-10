76066a7472b18070857403d23e514f0f28994c3d
Shrestha Jaiswal <shresthajaiswal1705@gmail.com>
2023-06-10 16:37:14 +0530

    STACK WITH ARRAY

--- Stack/stack_without_stl.cpp
+++ Stack/stack_without_stl.cpp
@@ -1,32 +1,60 @@
 #include <iostream>
 using namespace std;
 
-
-class stack
+class Stack
 {
-    public:
+public:
     int *arr;
     int size;
     int top;
 
-    stack(int size)
+    Stack(int size)
     {
-        this->size=size;
-        arr=new int[size];
-        top=-1;
+        this->size = size;
+        arr = new int[size];
+        top = -1;
     }
-
 
     void push(int element)
     {
-        if(top == size)
-            cout << "stack Overflow";
-        else{
+        if (top == size - 1)
+            cout << "Stack Overflow\n";
+        else
+        {
             top++;
-            arr[top]=element;
+            arr[top] = element;
         }
     }
-    void pop
-    void top
-    void size
+
+    void pop()
+    {
+        if (top > -1)
+            top--;
+        else
+            cout << "Stack Underflow\n";
+    }
+
+    void peek()
+    {
+        if (top > -1)
+            cout << arr[top] << endl;
+        else
+            cout << "No element in the stack\n";
+    }
+
+    bool empty()
+    {
+        return (top == -1);
+    }
+};
+
+int main(){
+
+    Stack stack(5);
+    stack.push(1);
+    stack.push(2);
+    stack.push(3);
+    stack.pop();
+    stack.peek();
+    
 }
