388749e7230aaefb292f67586e9d810bb2db8c86
Shrestha Jaiswal <shresthajaiswal1705@gmail.com>
2023-06-10 16:12:36 +0530

    stack without stl

--- Stack/stack.cpp
+++ Stack/stack.cpp
@@ -4,13 +4,15 @@ using namespace std;
 int main()
 {
     cout << "stacks practice" << endl;
+    //creation
     stack <int> s;
+    //push
     s.push(4);
-    s.push(4);
-    s.push(4);
-    s.push(4);
-    s.push(4);
-    s.push(4);
+    s.push(5);
+    s.push(6);
+    s.push(7);
+    s.push(2);
+    s.pop();
 
 cout << s.top() << endl;    
 cout << s.empty() << endl;
