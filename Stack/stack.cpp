5e8eda0e9a0678eef8cf02ec04f98507666aafa1
Shrestha Jaiswal <shresthajaiswal1705@gmail.com>
2023-06-10 16:02:22 +0530

    stack intro

---
+++ Stack/stack.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+int main()
+{
+    cout << "stacks practice" << endl;
+    stack <int> s;
+    s.push(4);
+    s.push(4);
+    s.push(4);
+    s.push(4);
+    s.push(4);
+    s.push(4);
+
+cout << s.top();    
+
+}
