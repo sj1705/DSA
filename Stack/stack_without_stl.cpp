0dc25998631016dccd9802fa987b7ee7784af539
Shrestha Jaiswal <shresthajaiswal1705@gmail.com>
2023-06-10 16:22:49 +0530

    push done in stack

--- Stack/stack_without_stl.cpp
+++ Stack/stack_without_stl.cpp
@@ -1,2 +1,32 @@
 #include <iostream>
 using namespace std;
+
+
+class stack
+{
+    public:
+    int *arr;
+    int size;
+    int top;
+
+    stack(int size)
+    {
+        this->size=size;
+        arr=new int[size];
+        top=-1;
+    }
+
+
+    void push(int element)
+    {
+        if(top == size)
+            cout << "stack Overflow";
+        else{
+            top++;
+            arr[top]=element;
+        }
+    }
+    void pop
+    void top
+    void size
+}
