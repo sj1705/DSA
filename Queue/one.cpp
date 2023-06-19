a53144287deb03173094eed7d1140846068cf98e
Shrestha Jaiswal <shresthajaiswal1705@gmail.com>
2023-06-19 18:36:12 +0530

    queue stl

--- Queue/one.cpp
+++ Queue/one.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
+#include <queue>
 using namespace std;
 
 int main()
 {
-    cout << "queue practice in this folder";
+    cout << "queue practice in this folder stl ";
+
+
+    queue <int> q;
+    q.push(11);
+    q.push(15);
+    q.push(13);
+    cout << "front of queue : " << q.front() << endl;
+
+    cout << "size of queue is : " << q.size() << endl;
+    q.pop();
+    cout << "size of queue is : " << q.size() << endl;
+    cout << q.empty() << endl << endl;
+    cout << "front of queue : " << q.front() << endl;
 }
