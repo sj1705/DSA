3c08b7e7b60375fedae46ea7ba8cc286ad2aecb6
Shrestha Jaiswal <shresthajaiswal1705@gmail.com>
2023-06-22 03:42:39 +0530

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	deleted:    0020-valid-parentheses/0020-valid-parentheses.cpp
    	deleted:    0225-implement-stack-using-queues/0225-implement-stack-using-queues.cpp
    	deleted:    0225-implement-stack-using-queues/NOTES.md
    	deleted:    0225-implement-stack-using-queues/README.md
    	deleted:    0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.cpp
    	deleted:    0232-implement-queue-using-stacks/NOTES.md
    	deleted:    0503-next-greater-element-ii/NOTES.md
    	modified:   leetcode questions/0020-valid-parentheses/0020-valid-parentheses.cpp
    	deleted:    leetcode questions/0020-valid-parentheses/README.md
    	modified:   leetcode questions/0225-implement-stack-using-queues/0225-implement-stack-using-queues.cpp
    	deleted:    leetcode questions/0232-implement-queue-using-stacks/README.md
    	renamed:    0503-next-greater-element-ii/0503-next-greater-element-ii.cpp -> leetcode questions/0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
    	renamed:    0020-valid-parentheses/NOTES.md -> leetcode questions/0503-next-greater-element-ii/NOTES.md
    	renamed:    0503-next-greater-element-ii/README.md -> leetcode questions/0503-next-greater-element-ii/README.md

--- leetcode questions/0225-implement-stack-using-queues/0225-implement-stack-using-queues.cpp
+++ leetcode questions/0225-implement-stack-using-queues/0225-implement-stack-using-queues.cpp
@@ -1,50 +1,29 @@
 class MyStack {
 public:
-queue<int>q1,q2;
     MyStack() {
-        
     }
-    
+        queue<int>q;
     void push(int x) {
-        q1.push(x);
+        q.push(x);
+        for(int i=0;i<q.size()-1;i++)
+        {
+            q.push(q.front());
+            q.pop();
+        }
     }
     
     int pop() {
-        while(q1.size()>1){
-            q2.push(q1.front());
-            q1.pop();
-        }
-        int ans=q1.front();
-        q1.pop();
-        while(!q2.empty()){
-            q1.push(q2.front());
-            q2.pop();
-        }
-        return ans;
-        
+        int num=q.front();
+        q.pop();
+        return num;
     }
     
     int top() {
-        while(q1.size()>1){
-            q2.push(q1.front());
-            q1.pop();
-        }
-        int ans=q1.front();
-        q1.pop();
-        q2.push(ans);
-        while(!q2.empty()){
-            q1.push(q2.front());
-            q2.pop();
-        }
-        return ans;
-        
+        return q.front();
     }
     
     bool empty() {
-        if(q1.size()==0)
-        return 1;
-
-        return 0;
+        return q.empty();
     }
 };
 
