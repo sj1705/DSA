23230bf8da7214d61fd89b9f0713e0b3bc08b821
Shrestha Jaiswal <78994771+sj1705@users.noreply.github.com>
2023-06-25 02:59:31 +0530

    Time: 335 ms (41.40%), Space: 134.7 MB (71.20%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -7,7 +7,7 @@ public:
         {
             if (!dq.empty()&& dq.front()==i-k)
                 dq.pop_front();
-            while(!dq.empty()  && nums[dq.back()] < nums[i])
+            while(!dq.empty()  && nums[dq.back()] <= nums[i])
                 dq.pop_back();
             
             dq.push_back(i);
