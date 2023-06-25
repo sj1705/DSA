d440c1dea8c9cccb5b006800e10aa9651f4c8c98
Shrestha Jaiswal <shresthajaiswal1705@gmail.com>
2023-06-25 22:43:33 +0530

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	deleted:    0146-lru-cache/0146-lru-cache.cpp
    	deleted:    0146-lru-cache/README.md
    	deleted:    0994-rotting-oranges/NOTES.md
    	renamed:    0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp -> leetcode questions/0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
    	renamed:    0084-largest-rectangle-in-histogram/NOTES.md -> leetcode questions/0084-largest-rectangle-in-histogram/NOTES.md
    	renamed:    0084-largest-rectangle-in-histogram/README.md -> leetcode questions/0084-largest-rectangle-in-histogram/README.md
    	modified:   leetcode questions/0146-lru-cache/0146-lru-cache.cpp
    	renamed:    0239-sliding-window-maximum/0239-sliding-window-maximum.cpp -> leetcode questions/0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
    	renamed:    0146-lru-cache/NOTES.md -> leetcode questions/0239-sliding-window-maximum/NOTES.md
    	renamed:    0239-sliding-window-maximum/README.md -> leetcode questions/0239-sliding-window-maximum/README.md
    	renamed:    0994-rotting-oranges/0994-rotting-oranges.cpp -> leetcode questions/0994-rotting-oranges/0994-rotting-oranges.cpp
    	renamed:    0239-sliding-window-maximum/NOTES.md -> leetcode questions/0994-rotting-oranges/NOTES.md
    	renamed:    0994-rotting-oranges/README.md -> leetcode questions/0994-rotting-oranges/README.md

--- leetcode questions/0146-lru-cache/0146-lru-cache.cpp
+++ leetcode questions/0146-lru-cache/0146-lru-cache.cpp
@@ -1,65 +1,75 @@
-struct Node{
-    int key;
-    int val;
-    Node* next = NULL;
-    Node* prev = NULL;
-    Node(int _key, int value){
-        key = _key;
-        val = value;
-    }
-};
-
 class LRUCache {
 public:
-    Node* head = new Node(-1,-1);
-    Node* tail = new Node(-1,-1);
-
-    unordered_map<int,Node*>mp;
+    class node {
+        public:
+        int key;
+        int val;
+        node *next;
+        node *prev;
+        node(int _key,int _val)
+        {
+            key=_key;
+            val=_val;
+        }
+    };
+    node *head=new node(-1,-1);
+    node *tail=new node(-1,-1);
     int cap;
+    unordered_map<int,node*>mp;
     LRUCache(int capacity) {
-        head->next = tail;
-        tail->prev = head;
-        cap = capacity;
+        cap=capacity;
+        head->next=tail;
+        tail->prev=head;
+    }
+    void addnode(node* newnode){
+    node* temp=head->next;
+    newnode->next= temp;
+    newnode->prev= head;
+    head->next= newnode;
+    temp->prev=newnode;
     }
 
-    void deleteNode(int key){
-        Node* node = mp[key];
-        Node* prevNode = node->prev;
-        Node* nextNode = node->next;
-        prevNode->next = nextNode;
-        nextNode->prev = prevNode;
-        mp.erase(key);
-    }
-
-    void insert(int key, int value){
-        Node* temp = head->next;
-        Node* newNode = new Node(key, value);
-        head->next = newNode;
-        newNode->next = temp;
-        temp->prev = newNode;
-        newNode->prev = head;
-        mp[key] = newNode;
+    void deletenode(node* delnode){
+    node* delprev= delnode->prev;
+    node* delnext= delnode->next;
+    delprev->next= delnext;
+    delnext->prev= delprev;
     }
     
-    int get(int key) {
-        if(mp.find(key) != mp.end()){
-            int value = mp[key]->val;
-            deleteNode(key);
-            insert(key,value);
-            return value;
-        }
-        return -1;
-    }
+int get(int key_) {
+  if(mp.find(key_)!=mp.end()){
+    node* resnode=mp[key_];
+    int res=resnode->val;
+    mp.erase(key_);
+    deletenode(resnode);
+    addnode(resnode);
+    mp[key_]=head->next;
+    return res;
+}
+    return -1;
     
-    void put(int key, int value) {
-        if(mp.find(key) != mp.end()){
-            deleteNode(key);
-        }
+}
 
-        if(mp.size() >= cap){
-            deleteNode(tail->prev->key);
-        }
 
-        insert(key, value);
+void put(int key_, int value) {
+    if(mp.find(key_)!=mp.end()){
+        node* existingnode= mp[key_];
+        mp.erase(key_);
+        deletenode(existingnode);
+        
     }
+    if(mp.size()==cap){
+        mp.erase(tail->prev->key);
+        deletenode(tail->prev);
+    }
+    addnode(new node(key_,value));
+    mp[key_]=head->next;
+}
 };
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache* obj = new LRUCache(capacity);
+ * int param_1 = obj->get(key);
+ * obj->put(key,value);
+ */
