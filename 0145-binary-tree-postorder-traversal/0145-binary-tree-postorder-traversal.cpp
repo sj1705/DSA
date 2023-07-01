94e342506eaa11e18e5157c8d0f4dee826008cdc
Shrestha Jaiswal <78994771+sj1705@users.noreply.github.com>
2023-07-01 18:35:51 +0530

    Time: 0 ms (100.00%), Space: 8.3 MB (81.30%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
@@ -1,25 +1,41 @@
-
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    vector<int> postorderTraversal(TreeNode* root) {
-        vector<int> postOrder;
-        if(root==NULL) return postOrder;
-        stack<TreeNode *>st1,st2;
-        st1.push(root);
-        while(!st1.empty())
-        {
-            root=st1.top();
-            st1.pop();
-            st2.push(root);
-            if(root->left !=NULL) st1.push(root->left);
-            if(root->right!=NULL) st1.push(root->right);
+    vector<int> postorderTraversal(TreeNode* cur) {
+
+  vector < int > postOrder;
+  if (cur == NULL) return postOrder;
+
+  stack < TreeNode * > st;
+  while (cur != NULL || !st.empty()) {
+
+    if (cur != NULL) {
+      st.push(cur);
+      cur = cur -> left;
+    } else {
+      TreeNode * temp = st.top() -> right;
+      if (temp == NULL) {
+        temp = st.top();
+        st.pop();
+        postOrder.push_back(temp -> val);
+        while (!st.empty() && temp == st.top() -> right) {
+          temp = st.top();
+          st.pop();
+          postOrder.push_back(temp -> val);
         }
-        while(!st2.empty())
-        {
-            postOrder.push_back(st2.top()->val);
-            cout << st2.top()->val << endl;
-            st2.pop();
-        }
-        return postOrder;
+      } else cur = temp;
+    }
+  }
+  return postOrder;
     }
 };
