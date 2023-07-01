82c09c15f1cb55361b717d71600abd033c1f1fee
Shrestha Jaiswal <shresthajaiswal1705@gmail.com>
2023-07-01 18:45:55 +0530

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	deleted:    0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
    	deleted:    0094-binary-tree-inorder-traversal/NOTES.md
    	deleted:    0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
    	deleted:    0144-binary-tree-preorder-traversal/NOTES.md
    	deleted:    0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
    	deleted:    0145-binary-tree-postorder-traversal/NOTES.md
    	modified:   leetcode questions/0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
    	modified:   leetcode questions/0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp

--- leetcode questions/0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ leetcode questions/0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -7,22 +7,56 @@
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+//  * };
+//  */
+// class Solution {
+// public:
+//     vector<int> inorderTraversal(TreeNode* root) {
+//         stack <TreeNode *> st;
+//         vector<int> inorder;
+//         st.push(root);
+//         TreeNode * node =root;
+//         while(true)
+//         {
+//             if(node!=NULL)
+//             {
+//                 st.push(node);
+//                 node=node->left;
+//             }
+//             else
+//             {
+//                 if(st.empty()==true) break;
+//                 node=st.top();
+//                 inorder.push_back(node->val);
+//                 node=node->right;
+//             }
+//         }
+//         return inorder;
+//     }
+    
+// };
+
 class Solution {
 public:
-    void fetch(TreeNode* root,vector<int> &ans)
-    {
-        if(root==NULL)
-            return;
-        fetch(root->left,ans);
-        ans.push_back(root->val);
-        fetch(root->right,ans);
-    }
     vector<int> inorderTraversal(TreeNode* root) {
-        vector<int> ans;
-        fetch(root,ans);
-        return ans;
+     stack<TreeNode*> st;
+        TreeNode *node=root;
+        vector<int> inorder;
+        while(true){
+            if(node!=NULL){
+                st.push(node);
+            node=node->left;
+        }
+        else{
+            if(st.empty()==true) break;
+            node=st.top();
+            st.pop();
+            inorder.push_back(node->val);
+            node=node->right;
+        }
+        }
+        cout<<"Inorder traversal:";
+        return inorder;
     }
-
+    
 };
--- leetcode questions/0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
+++ leetcode questions/0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
@@ -11,17 +11,21 @@
  */
 class Solution {
 public:
-    void fetch(TreeNode* root,vector<int> &ans)
-    {
-        if(root==NULL)
-            return;
-        ans.push_back(root->val);
-        fetch(root->left,ans);
-        fetch(root->right,ans);
-    }
     vector<int> preorderTraversal(TreeNode* root) {
         vector<int> ans;
-        fetch(root,ans);
+        if(root==NULL) return ans;
+        
+        stack<TreeNode *> st;
+        st.push(root);
+        while(!st.empty())
+        {
+            root=st.top();
+            st.pop();
+            ans.push_back(root->val);
+            if(root->right!=NULL) st.push(root->right);
+            if(root->left!=NULL) st.push(root->left);
+
+        }
         return ans;
     }
 };
