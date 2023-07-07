13795584a8528f64c240cb81920dd5ed6a6a41c4
Shrestha Jaiswal <shresthajaiswal1705@gmail.com>
2023-07-08 03:43:47 +0530

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	deleted:    0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.cpp
    	deleted:    0987-vertical-order-traversal-of-a-binary-tree/NOTES.md
    	renamed:    Left View of Binary Tree - GFG/README.md -> GFG questions/Left View of Binary Tree - GFG/README.md
    	renamed:    Left View of Binary Tree - GFG/left-view-of-binary-tree.cpp -> GFG questions/Left View of Binary Tree - GFG/left-view-of-binary-tree.cpp
    	renamed:    Right View of Binary Tree - GFG/README.md -> GFG questions/Right View of Binary Tree - GFG/README.md
    	renamed:    Right View of Binary Tree - GFG/right-view-of-binary-tree.cpp -> GFG questions/Right View of Binary Tree - GFG/right-view-of-binary-tree.cpp
    	renamed:    Top View of Binary Tree - GFG/README.md -> GFG questions/Top View of Binary Tree - GFG/README.md
    	renamed:    Top View of Binary Tree - GFG/top-view-of-binary-tree.cpp -> GFG questions/Top View of Binary Tree - GFG/top-view-of-binary-tree.cpp
    	renamed:    0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.cpp -> leetcode questions/0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.cpp
    	renamed:    0103-binary-tree-zigzag-level-order-traversal/NOTES.md -> leetcode questions/0103-binary-tree-zigzag-level-order-traversal/NOTES.md
    	renamed:    0103-binary-tree-zigzag-level-order-traversal/README.md -> leetcode questions/0103-binary-tree-zigzag-level-order-traversal/README.md
    	modified:   leetcode questions/0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.cpp
    	renamed:    0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp -> leetcode questions/0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
    	renamed:    0124-binary-tree-maximum-path-sum/NOTES.md -> leetcode questions/0199-binary-tree-right-side-view/NOTES.md
    	renamed:    0199-binary-tree-right-side-view/README.md -> leetcode questions/0199-binary-tree-right-side-view/README.md
    	renamed:    0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.cpp -> leetcode questions/0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.cpp
    	renamed:    0199-binary-tree-right-side-view/NOTES.md -> leetcode questions/0987-vertical-order-traversal-of-a-binary-tree/NOTES.md
    	renamed:    0987-vertical-order-traversal-of-a-binary-tree/README.md -> leetcode questions/0987-vertical-order-traversal-of-a-binary-tree/README.md

--- leetcode questions/0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.cpp
+++ leetcode questions/0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
-         int MaxpathSum(TreeNode* root, int & maxi ){
-             if(root==NULL)return 0;
-             int leftsum=max(0, MaxpathSum(root->left, maxi));
-             int rightsum=max(0,MaxpathSum(root->right, maxi));
-             maxi=max(maxi, leftsum+rightsum+ root->val);
-             
-             return root->val + max(leftsum, rightsum);
-             
-         }
-    
-    int maxPathSum(TreeNode* root) {
+    int MaxpathSum(TreeNode* root, int & maxi )
+    {
+        if(root==NULL)return 0;
+        int leftsum=max(0, MaxpathSum(root->left, maxi));
+        int rightsum=max(0,MaxpathSum(root->right, maxi));
+        maxi=max(maxi, leftsum+rightsum+ root->val); 
+        return root->val + max(leftsum, rightsum);
+    }
+    int maxPathSum(TreeNode* root) 
+    {
         int maxi=-1e9;
         MaxpathSum( root, maxi);
         return maxi;
