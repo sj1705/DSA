cb9628e6caeadfc27b7dd079642d36a0e22c76f4
Shrestha Jaiswal <78994771+sj1705@users.noreply.github.com>
2023-07-12 03:10:50 +0530

    Time: 53 ms (6.59%), Space: 30.7 MB (77.15%) - LeetHub

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.cpp
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.cpp
@@ -1,19 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
     int countNodes(TreeNode* root) {
-        if(!root)return 0;
-        int lh=0;
-        TreeNode*l=root,*r=root;
-        while(l){
-            lh++;
-            l=l->left;
+        if(root ==NULL) return 0;
+        int lh=findhl(root);
+        int rh=findhr(root);
+        if(lh==rh) return (1<<lh) -1;
+        
+        return 1+ countNodes(root->left)+countNodes(root->right);
+    }
+    int findhl(TreeNode * node)
+    {
+        int height=0;
+        while(node)
+        {
+            height++;
+            node=node->left;
         }
-        int rh=0;
-        while(r){
-            rh++;
-            r=r->right;
+        return height;
+    }    
+    int findhr(TreeNode * node)
+    {
+        int height=0;
+        while(node)
+        {
+            height++;
+            node=node->right;
         }
-        if(lh==rh)return (1<<lh)-1;
-        return 1+countNodes(root->left)+countNodes(root->right);
+        return height;
     }
 };
