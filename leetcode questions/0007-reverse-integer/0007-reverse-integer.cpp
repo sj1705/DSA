d521b562596ca493778075e445dc9b10876b4e22
Shrestha Jaiswal <shresthajaiswal1705@gmail.com>
2023-07-12 03:20:24 +0530

    On branch main Your branch is behind 'origin/main' by 17 commits, and can be fast-forwarded.   (use "git pull" to update your local branch)
    Changes to be committed:
    	deleted:    0007-reverse-integer/0007-reverse-integer.cpp
    	deleted:    0007-reverse-integer/README.md
    	deleted:    0863-all-nodes-distance-k-in-binary-tree/NOTES.md
    	modified:   leetcode questions/0007-reverse-integer/0007-reverse-integer.cpp
    	renamed:    0101-symmetric-tree/0101-symmetric-tree.cpp -> leetcode questions/0101-symmetric-tree/0101-symmetric-tree.cpp
    	renamed:    0007-reverse-integer/NOTES.md -> leetcode questions/0101-symmetric-tree/NOTES.md
    	renamed:    0101-symmetric-tree/README.md -> leetcode questions/0101-symmetric-tree/README.md
    	renamed:    0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp -> leetcode questions/0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
    	renamed:    0101-symmetric-tree/NOTES.md -> leetcode questions/0236-lowest-common-ancestor-of-a-binary-tree/NOTES.md
    	renamed:    0236-lowest-common-ancestor-of-a-binary-tree/README.md -> leetcode questions/0236-lowest-common-ancestor-of-a-binary-tree/README.md
    	renamed:    0257-binary-tree-paths/0257-binary-tree-paths.cpp -> leetcode questions/0257-binary-tree-paths/0257-binary-tree-paths.cpp
    	renamed:    0236-lowest-common-ancestor-of-a-binary-tree/NOTES.md -> leetcode questions/0257-binary-tree-paths/NOTES.md
    	renamed:    0257-binary-tree-paths/README.md -> leetcode questions/0257-binary-tree-paths/README.md
    	renamed:    0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.cpp -> leetcode questions/0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.cpp
    	renamed:    0257-binary-tree-paths/NOTES.md -> leetcode questions/0662-maximum-width-of-binary-tree/NOTES.md
    	renamed:    0662-maximum-width-of-binary-tree/README.md -> leetcode questions/0662-maximum-width-of-binary-tree/README.md
    	renamed:    0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.cpp -> leetcode questions/0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.cpp
    	renamed:    0662-maximum-width-of-binary-tree/NOTES.md -> leetcode questions/0863-all-nodes-distance-k-in-binary-tree/NOTES.md
    	renamed:    0863-all-nodes-distance-k-in-binary-tree/README.md -> leetcode questions/0863-all-nodes-distance-k-in-binary-tree/README.md

--- leetcode questions/0007-reverse-integer/0007-reverse-integer.cpp
+++ leetcode questions/0007-reverse-integer/0007-reverse-integer.cpp
@@ -6,7 +6,7 @@ public:
          r=r*10+x%10; // find remainder and add its to r
          x=x/10;     // Update the value of x
         }
-        if(r>INT_MAX || r<INT_MIN) return 0; // check 32 bit range if r is outside the range then return 0  
-        return int(r);  // if r in the 32 bit range return r
+        if(r>INT_MAX || r<INT_MIN) return 0; // check range if r is outside the range then return 0  
+        return int(r);  // if r in the 32 bit range then return r
     }
 }; 
