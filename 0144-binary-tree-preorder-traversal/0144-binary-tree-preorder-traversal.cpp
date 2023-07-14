1ce94b5d6119dfc83554cd95d4953d5300264dda
Shrestha Jaiswal <78994771+sj1705@users.noreply.github.com>
2023-07-15 04:56:30 +0530

    Time: 0 ms (100.00%), Space: 8.3 MB (92.27%) - LeetHub

---
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
@@ -0,0 +1,34 @@
+
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> inorder;
+        TreeNode *curr=root;
+        while(curr!=NULL)
+        {
+            if(curr->left==NULL)
+            {
+                inorder.push_back(curr->val);
+                curr=curr->right;
+            }
+            else{
+                TreeNode *prev=curr->left;
+                while(prev->right!=NULL && prev->right !=curr)
+                {
+                    prev=prev->right;
+                }
+                if(prev->right==NULL)
+                {
+                    prev->right=curr;
+                    inorder.push_back(curr->val);
+                    curr=curr->left;
+                }
+                else{
+                    prev->right=NULL;
+                    curr=curr->right;
+                }
+            }
+        }
+        return inorder;
+    }
+};
