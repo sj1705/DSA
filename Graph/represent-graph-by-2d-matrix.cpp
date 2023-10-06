efcd50552c9ed1e4020f23b025645ce4f0a24c50
Shrestha Jaiswal <shresthajaiswal1705@gmail.com>
2023-10-06 20:29:43 +0530

    Changes to be committed: 	renamed:    Lowest Common Ancestor in a Binary Tree - GFG/README.md -> GFG questions/Lowest Common Ancestor in a Binary Tree - GFG/README.md 	renamed:    Lowest Common Ancestor in a Binary Tree - GFG/lowest-common-ancestor-in-a-binary-tree.cpp -> GFG questions/Lowest Common Ancestor in a Binary Tree - GFG/lowest-common-ancestor-in-a-binary-tree.cpp 	modified:   Graph/represent-graph-by-2d-matrix.cpp 	modified:   Graph/represent-graph-by-2d-matrix.exe

--- Graph/represent-graph-by-2d-matrix.cpp
+++ Graph/represent-graph-by-2d-matrix.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
 using namespace std;
 int main()
-{
-    cout<< "hello";
+{   int m,n,x,y;
+    cin >> m >> n;
+    int a[m][n]={0};
+    for(int i=1;i<=m;i++)
+        {
+            cin >> x >> y;
+            a[x][y] = 1;
+            a[y][x] = 1;
+        }
+
     return 0;
 }
