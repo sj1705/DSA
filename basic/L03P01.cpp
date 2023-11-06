3fa7ad3c91bd48549259ab000578e76b04194f85
Shrestha Jaiswal <shresthajaiswal1705@gmail.com>
2023-11-07 02:54:59 +0530

    modified:   BT/treeincpp.exe 	modified:   basic/L03P01.cpp 	deleted:    basic/L03P01.exe

--- basic/L03P01.cpp
+++ basic/L03P01.cpp
@@ -2,11 +2,10 @@
 using namespace std;
 int main()
 {
-    int n, m;
+    int n;
     cout << "Enter value of n" << endl;
     cin >> n;
-    cout << "Enter value of m" << endl;
-    cin >> n >> m;
+
     cout << "The value of n is:" << n << endl;
-    cout << "The value of mmm is:" << m << endl;
+
 }
