ab30f512e6aabd9115836b45fbd3bf3379c5f806
Shrestha Jaiswal <shresthajaiswal1705@gmail.com>
2023-12-17 12:05:17 +0530

    Changes to be committed: 	new file:   SlidingWindw/1.cpp

---
+++ SlidingWindw/1.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#define ll long long
+
+int n;
+int arr[(int)1e5+5]
+
+int main()
+{
+    cin>>n;
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+}
