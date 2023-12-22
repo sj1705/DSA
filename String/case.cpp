e902fcfce077c1f215ca1710a31d26efe056c7b6
Shrestha Jaiswal <shresthajaiswal1705@gmail.com>
2023-12-22 19:39:24 +0530

    Changes to be committed: 	new file:   String/case.cpp 	new file:   String/case.exe

---
+++ String/case.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string name;
+    getline(cin, name);
+    int i;
+    for(i=0;'\0'!=name[i];i++){
+        name[i]=name[i]+32;
+    }
+    cout << "length of name is:" << i<<endl;
+    cout << name;
+
+}
