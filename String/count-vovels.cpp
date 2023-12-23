f03799229ddaacc043453af47555376a326d8100
Shrestha Jaiswal <shresthajaiswal1705@gmail.com>
2023-12-23 18:28:33 +0530

    Changes to be committed: 	modified:   String/count-vovels.cpp

--- String/count-vovels.cpp
+++ String/count-vovels.cpp
@@ -4,11 +4,16 @@ int main()
 {
     string name;
     getline(cin, name);
-    int i,vcount=0;;
+    int i,vcount=0;int words=1;
     for(i=0;'\0'!=name[i];i++){
         if(name[i]=='a'||name[i]=='e'||name[i]=='i'||name[i]=='o'||name[i]=='u')
         vcount++;
+        if(name[i]==' ')
+        words++;
+
+
     }
     cout << "length of name is:" << i<<endl;
     cout << "count of vowels:"<<vcount;
+    cout << "count of words:"<<words;
 }
