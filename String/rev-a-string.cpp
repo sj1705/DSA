32f3dc3ee965ceac9d34b0912c09b792a2d6fb90
Shrestha Jaiswal <shresthajaiswal1705@gmail.com>
2023-12-26 03:11:28 +0530

    Changes to be committed: 	new file:   String/rev-a-string.cpp 	new file:   String/rev-a-string.exe

---
+++ String/rev-a-string.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    string name,revname;
+    getline(cin, name);
+    int i;
+    int l=name.length();
+    revname.resize(l);
+
+    for(i=0;i<l;i++){
+        revname[l-i-1]=name[i];
+    }
+    cout << revname;
+
+}
