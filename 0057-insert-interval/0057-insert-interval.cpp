728d5934f4578f8e58fdc239d096ff1e82587732
Shrestha Jaiswal <78994771+sj1705@users.noreply.github.com>
2024-06-16 16:03:46 +0530

    Time: 12 ms (52.55%), Space: 21.2 MB (10.71%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -1,35 +1,26 @@
 class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
-        auto it = intervals.begin();
-           int flag =0;
-        while(it != intervals.end()) {
-            if((*it)[0] < newInterval[0]) {
-                ++it;
-            } else {
+        bool inserted = false;
+        for(auto it = intervals.begin(); it != intervals.end(); ++it) {
+            if((*it)[0] >= newInterval[0]) {
                 intervals.insert(it, newInterval);
-                    flag=1;
+                inserted = true;
                 break;
             }
         }
-if(flag==0)
-        intervals.push_back(newInterval);
-            
-            
-                    vector<vector<int>> ans;
-
-            
-                    vector<int> temp =intervals[0];
-        for(auto it: intervals)
-        {
-            if(it[0]<=temp[1])
-            {
-                temp[1]=max(it[1],temp[1]);
-            }
-            else
-            {
+        if(!inserted) {
+            intervals.push_back(newInterval);
+        }
+        
+        vector<vector<int>> ans;
+        vector<int> temp = intervals[0];
+        for(auto it: intervals) {
+            if(it[0] <= temp[1]) {
+                temp[1] = max(it[1], temp[1]);
+            } else {
                 ans.push_back(temp);
-                temp=it;
+                temp = it;
             }
         }
         ans.push_back(temp);
