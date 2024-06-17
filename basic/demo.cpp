055ea26f824ebdd8fdbbe206c5300067a49091e9
Shrestha Jaiswal <shresthajaiswal1705@gmail.com>
2024-06-17 14:40:34 +0530

    Changes to be committed: 	modified:   basic/demo.cpp 	modified:   basic/demo.exe

--- basic/demo.cpp
+++ basic/demo.cpp
@@ -1,21 +1,102 @@
+#include <iostream>
+using namespace std;
+
 class A {
 public:
-    void method() {
-        cout << "Method of Class A" << endl;
+    void methodA() {
+        cout << "Method of Class A" ;
     }
 };
 
 class B : public A {
 public:
     void methodB() {
-        cout << "Method of Class B" << endl;
+        cout << "Method of Class B" ;
     }
 };
 
 int main() {
-    // This is not allowed and will result in a compile-time error
-    A obj = new B();
-    
-    // If it were allowed, what would happen when we call a method that only exists in B?
-    obj.methodB(); // This method does not exist on an A object!
+    B* obj = new B(); // Correct: A pointer can point to an A object
+    obj->methodB(); // Correct way to call methodA on the object pointed by obj
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
