06926f83c8357bee442076035b491956d4529542
Shrestha Jaiswal <78994771+sj1705@users.noreply.github.com>
2024-08-06 00:05:48 +0530

    Time: 424 ms (24.13%), Space: 11 MB (79.97%) - LeetHub

---
+++ 0494-target-sum/0494-target-sum.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int findTargetSumWays(vector<int>& nums, int target) {
+        int n = nums.size();
+        return solve(n-1,nums,target);
+    }
+
+    int solve(int index, vector<int> &nums,int target)
+    {
+        if(index == 0)
+        {
+            /*
+            if(target == nums[index])
+            {
+                return 1;
+            }
+            return 0;
+            */
+            if(index==0){
+        if(target==0&&nums[0]==0)
+            return 2;
+        return (nums[0]==abs(target));
+             }
+        }
+        int plus =  solve(index-1,nums,target - nums[index]); 
+        int minimus =  solve(index-1,nums,target + nums[index]);
+
+        return ( plus + minimus);
+    }
+};
