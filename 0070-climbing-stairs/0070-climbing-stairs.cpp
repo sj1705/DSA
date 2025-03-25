2e9d1bd76e35e7e439b2c96fe706c0e9e78306b4
Shrestha Jaiswal <78994771+sj1705@users.noreply.github.com>
2025-03-25 23:14:46 +0530

    Time: 0 ms (100%), Space: 8.6 MB (17.16%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,21 +1,16 @@
 class Solution {
 public:
-    int f(int  n, vector <int> & dp)
-    {
-        if(n==0||n==1) return 1;
-        if(dp[n]!=-1) return dp[n];
-        int left=f(n-1,dp);
-        int  right=f(n-2,dp);
-        return dp[n]=right+left;
-    }
+//Space optimization
     int climbStairs(int n) {
         vector <int> dp(n+1,-1);
-        dp[0]=1;
-        dp[1]=1;
+       int prev2=1;
+       int prev=1;
         for(int  i=2;i<=n;i++)
         {
-            dp[i]=dp[i-1]+dp[i-2];
+            int curr=prev+prev2;
+            prev2=prev;
+            prev=curr;
         }
-        return dp[n];
+        return prev;
     }
 };
