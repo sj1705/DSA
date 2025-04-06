4fd27c1fccc7a906a886ce5cc44e2ff505fa23aa
Shrestha Jaiswal <78994771+sj1705@users.noreply.github.com>
2025-04-06 18:19:24 +0530

    Time: 0 ms (100%), Space: 13.3 MB (9.93%) - LeetHub

---
+++ 0120-triangle/0120-triangle.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+int  f(int i,int j, vector<vector<int>> & triangle,vector<vector<int>> & dp)
+{
+    if(i==triangle.size()-1)
+    {
+        return triangle[i][j]; 
+    }
+    if(dp[i][j]!=-1)
+        return dp[i][j];
+    int down=triangle[i][j]+f(i+1,j,triangle,dp);
+    int downRight=triangle[i][j]+f(i+1,j+1,triangle,dp);
+     
+     return dp[i][j]=min(down,downRight);
+}
+    int minimumTotal(vector<vector<int>>& triangle) {
+        int n=triangle.size();
+        int m=triangle[0].size();
+        vector<vector<int>> dp;
+        for (int i = 0; i < n; ++i) 
+            dp.push_back(vector<int>(triangle[i].size(), -1));
+        return f(0,0,triangle,dp);
+    }
+};
