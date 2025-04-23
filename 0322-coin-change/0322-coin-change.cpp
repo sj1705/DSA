266c3bcb67a09ff8e7b5ea4c11c9f27d754d1730
Shrestha Jaiswal <78994771+sj1705@users.noreply.github.com>
2025-04-23 23:13:53 +0530

    Time: 47 ms (33.78%), Space: 49 MB (34.37%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,26 +1,29 @@
 class Solution {
 public:
-    int f(int ind,int amount,vector<int> &coins,vector<vector<int>> &dp)
-    {
-        if(ind==0)
-        {
-            if(amount%coins[ind]==0) return amount/coins[ind];
-            else return 1e9;
-        }
-        if(dp[ind][amount]!=-1) return dp[ind][amount];
-
-        int ntake=0+f(ind-1,amount,coins,dp);
-        int  take=INT_MAX;
-        if(coins[ind]<=amount) 
-            take=1+f(ind,amount-coins[ind],coins,dp);
-        return dp[ind][amount]=min(take,ntake);
-    }
     int coinChange(vector<int>& coins, int amount) {
-        int n=coins.size();
-    vector<vector<int>> dp(n, vector<int>(amount + 1, -1));
-        int ans = f(n-1,amount,coins,dp);
-        if(ans >= 1e9)
-            return -1;
-        return ans;
+        int n = coins.size();
+        vector<vector<int>> dp(n, vector<int>(amount + 1, 0));
+
+        // Base case: fill the first row (only using the first coin)
+        for (int i = 0; i <= amount; i++) {
+            if (i % coins[0] == 0)
+                dp[0][i] = i / coins[0];
+            else
+                dp[0][i] = 1e9; // represent infinity
+        }
+
+        // Build the table for all coins and all amounts
+        for (int ind = 1; ind < n; ind++) {
+            for (int amt = 0; amt <= amount; amt++) {
+                int notTake = dp[ind - 1][amt];
+                int take = 1e9;
+                if (coins[ind] <= amt)
+                    take = 1 + dp[ind][amt - coins[ind]];
+                dp[ind][amt] = min(take, notTake);
+            }
+        }
+
+        int ans = dp[n - 1][amount];
+        return (ans >= 1e9) ? -1 : ans;
     }
 };
