c5fcef6e96c7830b8e0af9a2b27d34127be3a54c
Shrestha Jaiswal <78994771+sj1705@users.noreply.github.com>
2025-06-04 00:48:10 +0530

    Time: 17 ms (5.66%), Space: 27 MB (6.36%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,21 +1,16 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& v) {
-            int n = v.size(); //size of the array
-    vector<int> ls; // list of answers
+            int n = v.size(); 
+    vector<int> ls;
 
-    //declaring a map:
     map<int, int> mpp;
 
-    // least occurrence of the majority element:
     int mini = int(n / 3) + 1;
 
-    //storing the elements with its occurnce:
     for (int i = 0; i < n; i++) {
         mpp[v[i]]++;
 
-        //checking if v[i] is
-        // the majority element:
         if (mpp[v[i]] == mini) {
             ls.push_back(v[i]);
         }
