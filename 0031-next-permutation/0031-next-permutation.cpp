925db24cbd274b3a7b5bfdc6bb7b11b1534fbf32
Shrestha Jaiswal <78994771+sj1705@users.noreply.github.com>
2025-06-03 22:42:58 +0530

    Time: 0 ms (100%), Space: 15.6 MB (92.14%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,24 +1,34 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        int n = nums.size();
-        int i = n - 2;
+        int  n=nums.size();
+        int ind=-1;
 
-        // Step 1: Find the first decreasing element from the right
-        while (i >= 0 && nums[i] >= nums[i + 1]) {
-            i--;
-        }
-
-        if (i >= 0) {
-            // Step 2: Find element just larger than nums[i] from the right
-            int j = n - 1;
-            while (nums[j] <= nums[i]) {
-                j--;
+        for(int i=n-2;i>=0;i--)
+        {
+            if(nums[i]<nums[i+1])
+            {
+                 ind=i;
+                break;
             }
-            swap(nums[i], nums[j]);
         }
 
-        // Step 3: Reverse the suffix
-        reverse(nums.begin() + i + 1, nums.end());
+        if(ind==-1)
+        {
+            reverse(nums.begin(),nums.end());
+            return;
+        }
+
+
+        for(int i=n-1;i>=0;i--)
+        {
+            if(nums[i]>nums[ind])
+            {
+                swap(nums[i],nums[ind]);
+                break;
+            }
+        }
+        reverse(nums.begin()+ind+1,nums.end());
+        return;
     }
 };
