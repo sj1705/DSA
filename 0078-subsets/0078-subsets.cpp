468ca1875532bcdd53b2d60ef886ab68ae060d17
Shrestha Jaiswal <78994771+sj1705@users.noreply.github.com>
2025-06-05 16:03:07 +0530

    Time: 0 ms (100%), Space: 10 MB (60.97%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
-    vector<vector<int>> subsets(vector<int>& nums) {
-        int n = nums.size();
-        int bitMask = (1 << n);    
-        
-        vector<vector<int>> ans;
-        
-        for(int i = 0; i < bitMask; i++) {
-            vector<int> temp;
-            
-            for(int j = 0; j < n; j++) {
-                if((i & (1 << j)) != 0) { 
-				
-                    temp.push_back(nums[j]);
-                }
-            }
-            
-            ans.push_back(temp);
+    void f(int ind,vector<int> & temp, vector<vector<int>>& ans, vector<int>& nums )
+    {
+        ans.push_back(temp);
+        if(ind==nums.size())
+            return;
+        for(int i=ind;i<nums.size();i++)
+        {
+            temp.push_back(nums[i]);
+            f(i+1,temp,ans,nums);
+            temp.pop_back();
+            while(i+1<nums.size()&&nums[ind]==nums[i+1])
+                i++;
         }
-        
+        return;
+    }
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> ans;
+        vector<int> temp;
+        f(0,temp,ans,nums);
         return ans;
     }
 };
