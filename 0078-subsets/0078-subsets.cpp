21ccd4840ad7499fec9f5fa406152f2f22f3aac2
Shrestha Jaiswal <78994771+sj1705@users.noreply.github.com>
2025-06-05 15:39:08 +0530

    Time: 0 ms (100%), Space: 10 MB (60.97%) - LeetHub

---
+++ 0078-subsets/0078-subsets.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        int n = nums.size();
+        int bitMask = (1 << n);    
+        
+        vector<vector<int>> ans;
+        
+        for(int i = 0; i < bitMask; i++) {
+            vector<int> temp;
+            
+            for(int j = 0; j < n; j++) {
+                if((i & (1 << j)) != 0) { 
+				
+                    temp.push_back(nums[j]);
+                }
+            }
+            
+            ans.push_back(temp);
+        }
+        
+        return ans;
+    }
+};
