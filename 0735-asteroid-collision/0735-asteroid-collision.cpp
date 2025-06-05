55e95780d1949c460fd4814f65b55f2839258e89
Shrestha Jaiswal <78994771+sj1705@users.noreply.github.com>
2025-06-05 21:36:12 +0530

    Time: 1 ms (42.04%), Space: 21.6 MB (89.88%) - LeetHub

---
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& asteroids) {
+        vector<int> st;
+        for(int i=0;i<asteroids.size();i++)
+        {
+            if(asteroids[i]>0) 
+                st.push_back(asteroids[i]);
+            else{
+                while(!st.empty()&&st.back()>0&&st.back()<abs(asteroids[i]))
+                    st.pop_back();
+                if(!st.empty()&&st.back()==abs(asteroids[i]))
+                    st.pop_back();
+                else if(st.empty()|| st.back()<0)
+                    st.push_back(asteroids[i]);
+            }
+        }
+        return st;
+    }
+};
