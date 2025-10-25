3bfa35cac6bcc503bb04b581b7cf32ce0b3e248c
Shrestha Jaiswal <78994771+sj1705@users.noreply.github.com>
2025-10-25 17:27:31 +0530

    Time: 0 ms (100%), Space: 7.7 MB (86.15%) - LeetHub

--- 1817-calculate-money-in-leetcode-bank/1817-calculate-money-in-leetcode-bank.cpp
+++ 1817-calculate-money-in-leetcode-bank/1817-calculate-money-in-leetcode-bank.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
-    int totalMoney(int n) {
-        auto [q, r]=div(n, 7);
+    int totalMoney(int n) {  
+        int q = n / 7;
+        int r = n % 7;
         return 28*q+7*q*(q-1)/2+(2*q+r+1)*r/2;
     }
 };
