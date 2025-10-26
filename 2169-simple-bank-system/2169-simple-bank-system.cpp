7d07063a9665ddef4217581815f54682405ec731
Shrestha Jaiswal <78994771+sj1705@users.noreply.github.com>
2025-10-26 22:55:41 +0530

    Time: 4 ms (95.29%), Space: 123.7 MB (60.18%) - LeetHub

---
+++ 2169-simple-bank-system/2169-simple-bank-system.cpp
@@ -0,0 +1,34 @@
+class Bank {
+public:
+    vector <long long> balance;
+    int n;
+    Bank(vector<long long>& balance) {
+        n=balance.size();
+        this->balance=balance;
+    }
+    
+    bool transfer(int account1, int account2, long long money) {
+        if(account1 >n || account2 > n || balance[account1-1]<money)
+            return false;
+        balance[account1-1] -=money;
+        balance[account2-1] +=money;
+        return true;
+    }
+    
+    bool deposit(int account, long long money) {
+        if(account>n)
+            return false;
+        balance[account-1] +=money;
+        return true;
+    }
+    
+    bool withdraw(int account, long long money) {
+        if(account>n)
+            return false;
+        if(balance[account-1] -money>=0)
+            balance[account-1] -=money;
+        else 
+            return false;
+        return true;
+    }
+};
