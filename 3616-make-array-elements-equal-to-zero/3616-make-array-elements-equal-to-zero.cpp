ce105b1bf43806198657e38349674568d1e1fcb7
Shrestha Jaiswal <78994771+sj1705@users.noreply.github.com>
2025-10-28 17:35:35 +0530

    Time: 173 ms (18.35%), Space: 23.6 MB (20.64%) - LeetHub

--- 3616-make-array-elements-equal-to-zero/3616-make-array-elements-equal-to-zero.cpp
+++ 3616-make-array-elements-equal-to-zero/3616-make-array-elements-equal-to-zero.cpp
@@ -1,15 +1,53 @@
 class Solution {
 public:
     int countValidSelections(vector<int>& nums) {
-        int len = nums.size(), count = 0, left = 0, right = 0;
-        for (int v : nums) right += v;
-        for (int i = 0; i < len; i++) {
-            left += nums[i];
-            right -= nums[i];
-            if (nums[i] != 0) continue;
-            if (left == right) count += 2;
-            if (abs(left - right) == 1) count++;
+        int n = nums.size();
+        int ans =0;
+        for(int i =0;i<n;i++){
+            if(nums[i]!=0)continue;
+
+            //left jake dekho
+            vector<int>tmp = nums;
+            int curr =i;
+            bool left = true;
+            while(curr >=0 && curr<n){
+                if(tmp[curr]){
+                    tmp[curr]--;
+                    left= !left;
+                }
+
+                if(left){
+                    curr--;
+                }
+                else{
+                   curr++;
+                }
+
+            }
+            int zcount = count(tmp.begin(),tmp.end(),0);
+            if(zcount == n)ans++;
+
+            //right jake dekho
+            tmp = nums;
+            bool right = true;
+            curr=i;
+             while(curr >=0 && curr<n){
+                if(tmp[curr]){
+                    tmp[curr]--;
+                    right= !right;
+                }
+
+                if(right){
+                    curr++;
+                }
+                else{
+                   curr--;
+                }
+
+            }
+            zcount = count(tmp.begin(),tmp.end(),0);
+            if(zcount == n)ans++;
         }
-        return count;
+        return ans;
     }
 };
