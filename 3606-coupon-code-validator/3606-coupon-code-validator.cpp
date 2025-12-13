cce215c833d34f519ca67370678b8d479140fec0
Shrestha Jaiswal <78994771+sj1705@users.noreply.github.com>
2025-12-13 22:02:36 +0530

    Time: 3 ms (81.55%), Space: 69.3 MB (60.51%) - LeetHub

---
+++ 3606-coupon-code-validator/3606-coupon-code-validator.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    bool checkValidCode(string &code) {
+        if(code.empty())
+            return false;
+
+        for(char &ch : code) {
+            if(!isalnum(ch) && ch != '_') {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    vector<string> validateCoupons(vector<string>& code, vector<string>& businessLine, vector<bool>& isActive) {
+        
+        unordered_map<string, int> mp = {
+            {"electronics", 0},
+            {"grocery", 1},
+            {"pharmacy", 2},
+            {"restaurant", 3}
+        };
+
+        vector<pair<int, string>> temp; //{businessLineIntegerValue, code}
+
+        for (int i = 0; i < code.size(); i++) {
+            if (isActive[i] &&
+                mp.count(businessLine[i]) &&
+                checkValidCode(code[i])) {
+
+                temp.push_back({mp[businessLine[i]], code[i]});
+            }
+        }
+
+        sort(begin(temp), end(temp));
+        vector<string> result;
+
+        for(auto &it : temp) {
+            result.push_back(it.second);
+        }
+
+        return result;
+    }
+};
