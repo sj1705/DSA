a60344b81df2f3ce61de334ae32322b45687d800
Shrestha Jaiswal <78994771+sj1705@users.noreply.github.com>
2026-01-08 18:58:17 +0530

    Time: 0 ms (100%), Space: 7.8 MB (92.3%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,13 +1,28 @@
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-        long long res = 1;
-        int N = m + n - 2;
-        int K = min(m - 1, n - 1); 
-        
-        for (int i = 1; i <= K; i++) {
-            res = res * (N - i + 1) / i;
+        int N=m+n-2;
+        double p=1;
+        int var=min(m,n);
+        for(int i=1;i<var;i++)
+        {
+            p = p * (N - i + 1) / i;
+
         }
-        return (int)res;
+        return (int)p;
     }
 };
+
+// class Solution {
+// public:
+//     int uniquePaths(int m, int n) {
+//         long long res = 1;
+//         int N = m + n - 2;
+//         int K = min(m - 1, n - 1); 
+        
+//         for (int i = 1; i <= K; i++) {
+//             res = res * (N - i + 1) / i;
+//         }
+//         return (int)res;
+//     }
+// };
