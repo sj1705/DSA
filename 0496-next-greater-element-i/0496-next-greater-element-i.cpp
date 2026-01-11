669f2643a4bbf4493beb2b5357b583671b74b6f3
Shrestha Jaiswal <78994771+sj1705@users.noreply.github.com>
2026-01-11 23:39:43 +0530

    Time: 0 ms (100%), Space: 12.8 MB (35.08%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -1,35 +1,22 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        vector <int> nge(nums2.size(),-1);
-        stack<int> st;
-        for(int i=nums2.size()-1;i>=0;i--)
-        {
-            while(!st.empty() && st.top() <= nums2[i]) 
-            {
-                 st.pop();
-            }
-            if(!st.empty()) 
-            {
-                    nge[i] = st.top();
-            } 
-            else 
-            {
-                    nge[i] = -1;
-            }
-            st.push(nums2[i]);
-        }
-        vector<int> ans(nums1.size());
-        for(int i=0;i<nums1.size();i++)
-        {
-            for(int j=0;j<nums2.size();j++)
-            {
-                if(nums1[i]==nums2[j])
-                {
-                    ans[i]=nge[j];
-                }
-            }
-        }
-        return ans;
+unordered_map<int, int> mapping; // val -> next_greater
+stack<int> st;
+
+// Calculate NGE for nums2 and store in a map
+for(int i = nums2.size() - 1; i >= 0; i--) {
+    while(!st.empty() && st.top() <= nums2[i]) st.pop();
+    
+    mapping[nums2[i]] = st.empty() ? -1 : st.top();
+    st.push(nums2[i]);
+}
+
+// O(1) lookup for each element in nums1
+vector<int> ans;
+for(int x : nums1) {
+    ans.push_back(mapping[x]);
+}
+return ans;
     }
 };
