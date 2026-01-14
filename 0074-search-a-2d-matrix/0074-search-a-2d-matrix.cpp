fe20a949b9cc3f94f165e69505f94ae6b9a2d0b2
Shrestha Jaiswal <78994771+sj1705@users.noreply.github.com>
2026-01-14 18:03:53 +0530

    Time: 0 ms (100%), Space: 13.4 MB (42.25%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,42 +1,33 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int low = 0;
-        int high = matrix.size() - 1;
-        
-
-        int upperbound = matrix.size(); 
-
-        while(low <= high) {
-            int mid = low + (high - low) / 2;
-            if(matrix[mid][0] > target) { 
-                upperbound = mid;
-                high = mid - 1;
+        int low=0;
+        int high=matrix.size()-1;
+        int upperbound=matrix.size();
+        while(low<=high)
+        {
+            int mid=low+(high-low)/2;
+            if(matrix[mid][0]>target)
+            {
+                upperbound=mid;
+                high=mid-1;
             }
-            else if(matrix[mid][0] == target) {
+            else if(matrix[mid][0] == target) 
                 return true;
-            }
-            else {
-                low = mid + 1;
-            }
+            else low =mid+1;
         }
-
-        if(upperbound == 0) return false;
-
-        int targetRow = upperbound - 1;
-        
-        low = 0;
-        high = matrix[0].size() - 1;
-        while(low <= high) {
-            int mid = low + (high - low) / 2;
-            if(matrix[targetRow][mid] == target)
+        if(upperbound==0) return false;
+        low=0;
+        high=matrix[0].size()-1;
+        while(low<=high)
+        {
+            int mid=low+(high-low)/2;
+            if(matrix[upperbound-1][mid]==target)
                 return true;
-            else if(matrix[targetRow][mid] < target)
-                low = mid + 1;
-            else 
-                high = mid - 1;
+            else if(matrix[upperbound-1][mid]<target)
+                low=mid+1;
+            else high=mid-1;
         }
-        
         return false;
     }
 };
