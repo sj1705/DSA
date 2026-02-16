077d6d7eb359ef0b172b0d9e15ba8c77f71e9e8d
Shrestha Jaiswal <78994771+sj1705@users.noreply.github.com>
2026-02-16 12:23:15 +0530

    Time: 4 ms (29.28%), Space: 8.2 MB (73.18%) - LeetHub

---
+++ 0190-reverse-bits/0190-reverse-bits.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int reverseBits(int n) {
+        if (n == 0) return 0;
+    
+        int result = 0;
+        for (int i = 0; i < 32; i++) {
+            result <<= 1;
+            
+
+            result = (result | (n & 1));
+            
+            n >>= 1;
+        }
+
+        return result;
+    }
+};
+
